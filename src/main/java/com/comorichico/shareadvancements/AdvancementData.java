0118ff81635a77094bd419e0c0a1f497f1f72453
comorichico <chico@comorichico.com>
2023-06-24 18:20:01 +0900

    完成に近づいているが進捗途中の状況が正しく追加されないバグあり

---
+++ src/main/java/com/comorichico/shareadvancements/AdvancementData.java
@@ -0,0 +1,75 @@
+package com.comorichico.shareadvancements;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AdvancementData {
+    private String key; // 進捗を表すキー
+    private String value; // 進捗の内容
+    private Collection<String> awardedCriteria; // 達成済みの進捗の集合
+    private Collection<String> remainingCriteria; // 未達成の進捗の集合
+    private boolean isDone; // 達成済みフラグ
+
+    public AdvancementData(String key, String value, Collection<String> awardedCriteria, Collection<String> remainingCriteria, boolean isDone) {
+        this.key = key;
+        this.value = value;
+        this.awardedCriteria = awardedCriteria;
+        this.remainingCriteria = remainingCriteria;
+        this.isDone = isDone;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public Collection<String> getAwardedCriteria() {
+        return awardedCriteria;
+    }
+
+    public Collection<String> getRemainingCriteria() {
+        return remainingCriteria;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setAwardedCriteria(Collection<String> awardedCriteria) {
+        if(null == this.awardedCriteria){
+            this.awardedCriteria = awardedCriteria;
+        } else {
+            Stream.concat(this.awardedCriteria.stream(),awardedCriteria.stream())
+                    .distinct()
+                    .collect(Collectors.toList());
+        }
+    }
+
+    public void setRemainingCriteria(Collection<String> remainingCriteria) {
+        if(null == this.remainingCriteria){
+            this.remainingCriteria = remainingCriteria;
+        } else {
+            Stream.concat(this.remainingCriteria.stream(),remainingCriteria.stream())
+                    .distinct()
+                    .collect(Collectors.toList());;
+        }
+    }
+
+    public void setDone(boolean isDone) {
+        if(isDone){
+            this.isDone = true;
+        }
+    }
+}
--- src/main/java/com/comorichico/shareadvancements/ShareAdvancements.java
+++ src/main/java/com/comorichico/shareadvancements/ShareAdvancements.java
@@ -1,17 +1,94 @@
 package com.comorichico.shareadvancements;
 
+import com.destroystokyo.paper.event.player.PlayerAdvancementCriterionGrantEvent;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.World;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.advancement.AdvancementProgress;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ShareAdvancements extends JavaPlugin {
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
+
+public final class ShareAdvancements extends JavaPlugin implements Listener {
+
+    private List<AdvancementData> advancements = new ArrayList<>();
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getAdvancementData();
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void getAdvancementData() {
+        Gson gson = new Gson();
+
+        InputStreamReader reader = new InputStreamReader(
+                ShareAdvancements.class.getClassLoader().getResourceAsStream("ja_jp.json"),
+                StandardCharsets.UTF_8
+        );
+
+        JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+
+        if (jsonObject != null) {
+            for (String key : jsonObject.keySet()) {
+                if (key.startsWith("advancements") && key.endsWith("title")) {
+                    String value = jsonObject.get(key).getAsString();
+                    AdvancementData data = new AdvancementData(key.replace("advancements.", "").replace(".title", "").replace(".", "/"), value, null, null, false);
+                    this.advancements.add(data);
+                }
+            }
+        }
+    }
+
+    public void setAdvancementData(Player player){
+        for(AdvancementData advancementData: this.advancements){
+            // 進捗オブジェクトを取得する
+            Advancement advancement = Bukkit.getServer().getAdvancement(NamespacedKey.minecraft(advancementData.getKey()));
+            if(null != advancement){
+                // プレイヤーの進捗オブジェクトを取得する
+                AdvancementProgress progress = player.getAdvancementProgress(advancement);
+
+                // 進捗の進行度をセットする
+                advancementData.setAwardedCriteria(progress.getAwardedCriteria());
+                advancementData.setRemainingCriteria(progress.getRemainingCriteria());
+                advancementData.setDone(progress.isDone());
+            }
+        }
+    }
+    @EventHandler
+    public void onPlayerAdvancementCriterionGrant(PlayerAdvancementCriterionGrantEvent event) {
+        ///advancement revoke @a everything
+
+        setAdvancementData(event.getPlayer());
+        for(AdvancementData advancementData: this.advancements){
+            if(advancementData.isDone()){
+                event.getPlayer().sendMessage(Component.text("達成済み：" + advancementData.getValue()));
+            } else {
+                if(null != advancementData.getAwardedCriteria()){
+                    for(String s: advancementData.getAwardedCriteria()){
+                        event.getPlayer().sendMessage(Component.text("進捗途中：" + advancementData.getValue() + "：" + s));
+                        for(World w: Bukkit.getServer().getWorlds()){
+                            for(Player p: w.getPlayers()){
+                                Advancement advancement = Bukkit.getServer().getAdvancement(NamespacedKey.minecraft(advancementData.getKey()));
+                                if(null != advancement){
+                                    AdvancementProgress progress = p.getAdvancementProgress(advancement);
+                                    progress.awardCriteria(s);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
